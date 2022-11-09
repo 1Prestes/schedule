37fbff23c6bb71424e93b4a3991a9f6257db9d25
Dijango Alves Rodolfo <dijango_ilha@hotmail.com>
2022-11-08 23:16:39 -0300

    feat: add create method

--- src/main/java/com/scheduleapi/controller/UserController.java
+++ src/main/java/com/scheduleapi/controller/UserController.java
@@ -7,12 +7,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
@@ -35,5 +31,10 @@ public class UserController {
     @GetMapping(path = "/{id}")
     public ResponseEntity<User> findById(@PathVariable UUID id) {
         return ResponseEntity.ok(userService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<User> save(@RequestBody User user) {
+        return new ResponseEntity(userService.save(user), HttpStatus.CREATED);
     }
 }
--- src/main/java/com/scheduleapi/service/UserService.java
+++ src/main/java/com/scheduleapi/service/UserService.java
@@ -6,30 +6,33 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 @Service
 public class UserService {
 
-    private List<User> users = List.of(new User(
-                    UUID.fromString("e4e9413e-7783-46c1-8293-9b56f6ba69f0"),
-                    "El Kuririn",
-                    "Rua dos bobos",
-                    "kuririn",
-                    "show",
-                    LocalDate.now()
-            ),
-            new User(
-                    UUID.fromString("5d98a9ba-3b1b-4a9a-8519-f6e156f0c334"),
-                    "El Goku",
-                    "Rua dos marajas",
-                    "goku",
-                    "dbz",
-                    LocalDate.now()
-            )
-    );
-
+    private static List<User> users;
+    static {
+        users = new ArrayList<>(List.of(new User(
+                        UUID.fromString("e4e9413e-7783-46c1-8293-9b56f6ba69f0"),
+                        "El Kuririn",
+                        "Rua dos bobos",
+                        "kuririn",
+                        "show",
+                        LocalDate.now()
+                ),
+                new User(
+                        UUID.fromString("5d98a9ba-3b1b-4a9a-8519-f6e156f0c334"),
+                        "El Goku",
+                        "Rua dos marajas",
+                        "goku",
+                        "dbz",
+                        LocalDate.now()
+                )
+        ));
+    }
     public List<User> listAll() {
         return users;
     }
@@ -39,5 +42,12 @@ public class UserService {
                 .filter(user -> user.getIduser().equals(id))
                 .findFirst()
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "User not found."));
+    }
+
+    public User save(User user) {
+        user.setIduser(UUID.randomUUID());
+        users.add(user);
+        
+        return user;
     }
 }
